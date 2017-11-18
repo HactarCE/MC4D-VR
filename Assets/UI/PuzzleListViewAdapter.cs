8d610c222022378060ca2f7e1c340d911e4a3c19
Hactar <HactarCE@users.noreply.github.com>
2017-11-18 16:52:13 -0500

    :construction: Added UI mockup

---
+++ Assets/UI/PuzzleListViewAdapter.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PuzzleListViewAdapter : MonoBehaviour {
+
+	public Button contentPrefab;
+
+	// Use this for initialization
+	void Start () {
+		string[] puzzles = new string[] {
+			"{3,3,3} Simplex",
+			"{3}x{4} Triangular Duoprism",
+			"{4,3,3} Hypercube",
+			"{5}x{4} Pentagonal Duoprism",
+			"{6}x{4} Hexagonal Duoprism",
+			"{7}x{4} Heptagonal Duoprism",
+			"{8}x{4} Octogonal Duoprism",
+			"{9}x{4} Nonogonal Duoprism",
+			"{10}x{4} Decagonal Duoprism",
+			"{3}x{3}",
+			"{3}x{5}",
+			"{5}x{10}",
+			"{5}x{5}",
+			"{6}x{6}",
+			"{7}x{7}",
+			"{8}x{8}",
+			"{9}x{9}",
+			"{10}x{10}",
+			"{5,3}x{} Dodecahedral Prism",
+			"{5,3,3} Hypermegaminx (BIG!)",
+			"Invent your own!"
+		};
+		foreach (string puzzle in puzzles) {
+			Button newContentThing = Instantiate<Button> (contentPrefab, transform);
+			Debug.Log (newContentThing);
+			newContentThing.GetComponentInChildren<Text> ().text = " " + puzzle;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
---
+++ Assets/UI/SliderScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderScript : MonoBehaviour {
+
+	public Text valueLabel;
+	public float minValue;
+	public float maxValue;
+	public float defaultValue;
+
+	public delegate float roundValue(float unrounded);
+	public roundValue valueRounder = new roundValue (delegate(float unrounded) {
+		return Mathf.Round (100 * unrounded) / 100;
+	});
+
+	// Use this for initialization
+	void Start () {
+		this.value = defaultValue;
+		GetComponent<Slider> ().onValueChanged.AddListener (delegate(float arg0) {
+			updateValueLabel ();
+		});
+	}
+
+	public float value {
+		get {
+			return valueRounder(GetComponent<Slider> ().value * (maxValue - minValue) + minValue);
+		}
+		set {
+			GetComponent<Slider> ().value = (value - minValue) / (maxValue - minValue);
+			updateValueLabel ();
+		}
+	}
+
+	public void updateValueLabel() {
+		valueLabel.text = this.value.ToString();
+	}
+
+}
